03e1f308c8f67f4e12201a552099bbc7429b0741
Michael Tutt <tutt.michael@gmail.com>
2017-04-28 14:56:37 -0700

    updated styling for onFocus

--- src/File.js
+++ src/File.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 
 class File extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      inFocus: false
+    }
+    this.handleFocus = this.handleFocus.bind(this);
+  }
+  handleFocus() {
+    let newFocusState = !this.state.inFocus;
+    this.setState({
+      inFocus: newFocusState
+    });
+  }
   render() {
     let {props} = this;
     let {item, level} = props;
     let nextLevel = level + 1;
     let offset = level * 10;
 
+    let focusStyle = '';
+
+    if (this.state.inFocus) {
+      focusStyle = 'focus-file';
+    }
+
+    let itemStyle = `has-text-left file-style`;
+    let lineStyle = `spacing ${focusStyle}`;
+
     return (
-      <div className="has-text-left file-style" style={{'margin-left': `${offset}px`}}>
-        <div className="icon-sprite placeholder"></div>
-        <span className="icon-sprite list"></span>
-        <span>{item.name}</span>
+      <div className={itemStyle} style={{'margin-left': `${offset}px`}} onClick={this.handleFocus}>
+        <span className={lineStyle}>
+          <span className="icon-sprite placeholder"></span>
+          <span className="icon-sprite list"></span>
+          <span>{item.name}</span>
+        </span>
       </div>
     );
   }
--- src/Folder.js
+++ src/Folder.js
@@ -5,28 +5,31 @@ class Folder extends Component {
   constructor() {
     super();
     this.state = {
-      visible: false
+      visible: false,
+      inFocus: false
     }
     this.handleExpansionClick = this.handleExpansionClick.bind(this);
-  }
-
-  componentDidMount() {
-
+    this.handleFocus = this.handleFocus.bind(this);
   }
 
   handleExpansionClick() {
-    console.log('Expansion clicked');
-    let newVisibility = !this.state.visible
+    let newVisibility = !this.state.visible;
     this.setState({
       visible: newVisibility
-    })
+    });
+  }
+
+  handleFocus() {
+    let newFocusState = !this.state.inFocus;
+    this.setState({
+      inFocus: newFocusState
+    });
   }
 
   render() {
     let {props} = this;
     let {item, level} = props;
     let {children, type, isPrivate} = item;
-    console.log(item);
     let nextLevel = level + 1;
     let offset = level * 10;
 
@@ -46,22 +49,29 @@ class Folder extends Component {
     }
 
     let folderType = `folder`;
-    if (type) {
-
-    }
-
     if (isPrivate) {
       folderType = 'private-folder';
     }
+    let focusStyle = '';
 
+    if (this.state.inFocus) {
+      focusStyle = 'focus-file';
+    }
+
+    let itemStyle = `has-text-left file-style`;
     let expansionStyle = `icon-sprite ${expansionIcon}`;
     let folderStyle = `icon-sprite ${folderType}`;
+    let lineStyle = `spacing ${focusStyle}`;
 
     return (
-      <div className="has-text-left file-style" style={{'margin-left': `${offset}px`}}>
-        <span className={expansionStyle} onClick={this.handleExpansionClick}></span>
-        <span className={folderStyle}></span>
-        <span>{item.name}</span>
+      <div className={itemStyle}
+           style={{'margin-left': `${offset}px`}}
+           onClick={this.handleFocus}>
+        <span className={lineStyle}>
+          <span className={expansionStyle} onClick={this.handleExpansionClick}></span>
+          <span className={folderStyle}></span>
+          <span>{item.name}</span>
+        </span>
         <ul style={visibility}>
           {
             children.map(item => {
